40ee36fb0592be0a010b46066dd3cd5dbd5c4ed7
iRosSoftware <iros0522@gmail.com>
2018-12-06 19:35:37 +0900

    Build Test

---
+++ Assets/BuildClass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BuildClass
+{
+    public static void Build()
+	{
+		EditorBuildSettingsScene scene = new EditorBuildSettingsScene("Assets/Scenes/SampleScene.unity", true) ;
+
+		EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[1];
+		scenes[0] = scene;
+        // 実行
+	    var builds = BuildPipeline.BuildPlayer(scenes,"/mnt/ramdisk/test",BuildTarget.StandaloneLinuxUniversal,BuildOptions.None);
+    }
+}
